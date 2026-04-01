e9af01d41b5b20dffb8bf8bf6b88c1d9b18a2e2b
deanbunn <deanbunn@gmail.com>
2026-03-31 19:19:38 -0700

    Game Results

--- daylengame.py
+++ daylengame.py
@@ -9,6 +9,12 @@ def main():
     #Display Start Message
     print("Let's start the game! \n")
 
+    #Var for Game Status
+    gameStatus = ""
+
+    #Var for Total Rounds
+    totalRounds = 0
+
     #Create Entrant 1
     entrant1 = game_resources.Entrant(name="Raymond",strength=15)
     print(entrant1.showstats())
@@ -17,7 +23,13 @@ def main():
     entrant2 = game_resources.Entrant(name="Daylen",speed=15,intelligence=20)
     print(entrant2.showstats())
 
-    for i in range(1,11):
+    #While loop for entrants health
+    while entrant1.health > 0 and entrant2.health > 0:
+
+        #increment total rounds by 1
+        totalRounds += 1
+
+        #Rolling Attack Rolls for the round. Incrementing Wins or Health and Health Status
         if entrant1.attackroll() >= entrant2.attackroll():
             entrant1.wins += 1
             entrant2.health -= 5
@@ -25,11 +37,21 @@ def main():
             entrant2.wins += 1
             entrant1.health -= 5
 
+        #Checking for Entrants Health
+        if entrant1.health <= 0:
+            gameStatus = entrant1.name + " lost the game!"
+
+        if entrant2.health <= 0:
+            gameStatus = entrant2.name + " lost the game!"
     
-    print(entrant1.name + " won " + str(entrant1.wins) + " matches ")
-    print(entrant1.name + " health is " + str(entrant1.health))
-    print(entrant2.name + " won " + str(entrant2.wins) + " matches ")
-    print(entrant2.name + " health is " + str(entrant2.health))
+    
+    #reporting the outcome of the match
+    print("Played a total of " + str(totalRounds) + " rounds!")
+    print(entrant1.name + " won " + str(entrant1.wins) + " rounds" + " health is " + str(entrant1.health))
+    print(entrant2.name + " won " + str(entrant2.wins) + " rounds" + " health is " + str(entrant2.health))
+    print("\n")
+    print(gameStatus)
+    print("\n")
 
 
 #Check to See If Script Ran Directly Instead of Imported
